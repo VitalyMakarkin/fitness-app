62953630cbe28583f6888404a51d51976f9669ff
MakarkinVM <makarkinvm@nknh.sibur.ru>
2023-08-16 21:52:36 +0300

    Change navigation

--- app/src/main/java/com/example/fitness/navigation/FitnessAppNavHost.kt
+++ app/src/main/java/com/example/fitness/navigation/FitnessAppNavHost.kt
@@ -3,15 +3,17 @@ package com.example.fitness.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
+import com.example.fitness.feature.activity.navigation.activityRoute
+import com.example.fitness.feature.activity.navigation.activityScreen
 import com.example.fitness.ui.FitnessAppState
 import com.example.fitness.feature.exercisehistory.navigation.exerciseHistoryScreen
-import com.example.fitness.feature.exercisehistory.navigation.exerciseHistoryRoute
+import com.example.fitness.feature.schedule.navigation.scheduleScreen
 
 @Composable
 fun FitnessAppNavHost(
     appState: FitnessAppState,
     modifier: Modifier = Modifier,
-    startDestination: String = exerciseHistoryRoute
+    startDestination: String = activityRoute
 ) {
     val navController = appState.navHostController
     NavHost(
@@ -19,6 +21,8 @@ fun FitnessAppNavHost(
         startDestination = startDestination,
         modifier = modifier
     ) {
+        activityScreen()
+        scheduleScreen()
         exerciseHistoryScreen()
     }
 }
--- app/src/main/java/com/example/fitness/ui/FitnessAppState.kt
+++ app/src/main/java/com/example/fitness/ui/FitnessAppState.kt
@@ -3,7 +3,9 @@ package com.example.fitness.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
 @Composable
@@ -19,5 +21,7 @@ fun rememberFitnessAppState(
 class FitnessAppState(
     val navHostController: NavHostController
 ) {
-    // TODO
+    val currentDestination: NavDestination?
+        @Composable get() = navHostController
+            .currentBackStackEntryAsState().value?.destination
 }
