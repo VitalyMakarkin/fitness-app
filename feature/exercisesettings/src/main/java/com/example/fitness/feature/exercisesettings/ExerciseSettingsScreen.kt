1cd54970337da5e838f0dc7761a115d7dd01a4aa
MakarkinVM <makarkinvm@nknh.sibur.ru>
2023-09-04 21:34:30 +0300

    Update exercise settings ui

--- feature/exercisesettings/src/main/java/com/example/fitness/feature/exercisesettings/ExerciseSettingsScreen.kt
+++ feature/exercisesettings/src/main/java/com/example/fitness/feature/exercisesettings/ExerciseSettingsScreen.kt
@@ -1,11 +1,21 @@
 package com.example.fitness.feature.exercisesettings
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -37,29 +47,70 @@ internal fun ExerciseSettingsScreen(
     modifier: Modifier = Modifier
 ) {
     when (uiState) {
-        is ExerciseSettingsUiState.Success -> Column {
-            Text(
-                text = "${uiState.activitiesCount} exercises completed",
-                modifier = modifier.clickable {
-                    onExerciseHistoryClick()
-                }
-            )
-            Text(
-                text = "${uiState.exerciseCategoriesCount} exercise categories",
-                modifier = modifier.clickable {
-                    onExerciseCategoriesClick()
-                }
-            )
-            Text(
-                text = "${uiState.exerciseGroupsCount} exercise groups",
-                modifier = modifier.clickable {
-                    onExerciseGroupsClick()
-                }
-            )
+        is ExerciseSettingsUiState.Success -> LazyVerticalGrid(
+            modifier = modifier.fillMaxSize(),
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            item {
+                SettingsGridItem(
+                    title = "Exercises completed",
+                    subtitle = uiState.activitiesCount.toString(),
+                    onClick = { onExerciseHistoryClick() }
+                )
+            }
+            item {
+                SettingsGridItem(
+                    title = "Exercises categories",
+                    subtitle = uiState.exerciseCategoriesCount.toString(),
+                    onClick = { onExerciseCategoriesClick() }
+                )
+            }
+            item {
+                SettingsGridItem(
+                    title = "Exercises groups",
+                    subtitle = uiState.exerciseGroupsCount.toString(),
+                    onClick = { onExerciseGroupsClick() }
+                )
+            }
         }
 
         is ExerciseSettingsUiState.Loading -> Text(
             text = "ExerciseSettings: Loading!"
         )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun SettingsGridItem(
+    modifier: Modifier = Modifier,
+    title: String,
+    subtitle: String = "",
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = modifier
+            .height(200.dp),
+        onClick = onClick
+    ) {
+        Text(
+            text = title,
+            fontSize = 14.sp, // TODO: move to theme typography
+            fontWeight = FontWeight(800), // TODO: move to theme typography
+            modifier = modifier
+                .padding(start = 8.dp, top = 8.dp)
+        )
+        if (subtitle.isNotEmpty()) {
+            Text(
+                text = subtitle,
+                fontSize = 20.sp, // TODO: move to theme typography
+                fontWeight = FontWeight(800), // TODO: move to theme typography
+                modifier = modifier
+                    .padding(start = 8.dp)
+            )
+        }
     }
 }
