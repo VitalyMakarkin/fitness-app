434e1fb986700e426bf8571c6b346c9e875321e3
MakarkinVM <makarkinvm@nknh.sibur.ru>
2023-09-07 22:41:05 +0300

    Update ui

--- feature/savecompletedexercise/src/main/java/com/example/fitness/feature/savecompletedexercise/dialog/exercisecategoryselection/ExerciseCategorySelectionDialog.kt
+++ feature/savecompletedexercise/src/main/java/com/example/fitness/feature/savecompletedexercise/dialog/exercisecategoryselection/ExerciseCategorySelectionDialog.kt
@@ -1,8 +1,10 @@
 package com.example.fitness.feature.savecompletedexercise.dialog.exercisecategoryselection
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
@@ -11,10 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.fitness.core.design.component.TopNavigationBar
 import com.example.fitness.core.model.ExerciseCategory
 
 @Composable
@@ -49,21 +55,55 @@ internal fun ExerciseCategorySelectionDialog(
         Surface(
             modifier = modifier.fillMaxSize()
         ) {
-            if (uiState is ExerciseCategorySelectionUiState.Success) {
-                LazyColumn(modifier = modifier) {
+            LazyColumn(
+                modifier = modifier.fillMaxSize()
+            ) {
+                item {
+                    TopNavigationBar(
+                        title = "Select category",
+                        onBackClick = { onDismiss() }
+                    )
+                }
+                if (uiState is ExerciseCategorySelectionUiState.Success) {
                     items(uiState.categories) { category ->
-                        Text(
-                            text = category.name,
-                            modifier = modifier
-                                .clickable {
-                                    onExerciseCategoryClicked(category)
-                                    onDismiss()
-                                }
-                                .fillMaxWidth()
+                        ExerciseCategoriesListItem(
+                            modifier = modifier.clickable {
+                                onExerciseCategoryClicked(category)
+                                onDismiss()
+                            },
+                            name = category.name,
+                            description = category.description ?: ""
                         )
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+internal fun ExerciseCategoriesListItem(
+    modifier: Modifier = Modifier,
+    name: String = "",
+    description: String = "",
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
+    ) {
+        Text(
+            text = name,
+            fontSize = 20.sp,
+            fontWeight = FontWeight(600)
+        )
+
+        if (description.isNotEmpty()) {
+            Text(
+                text = description,
+                fontSize = 20.sp,
+                fontWeight = FontWeight(400)
+            )
         }
     }
 }
