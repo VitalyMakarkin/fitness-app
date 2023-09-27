0463871cc638539e90e88845514a4d790e02f31d
MakarkinVM <makarkinvm@nknh.sibur.ru>
2023-09-27 14:02:12 +0300

    Update tests

---
+++ core/testing/src/main/java/com/example/fitness/core/testing/data/ExercisesData.kt
@@ -0,0 +1,61 @@
+package com.example.fitness.core.testing.data
+
+import com.example.fitness.core.model.Exercise
+
+val ExercisesData = listOf(
+    Exercise(
+        id = 1,
+        name = "Exercise 1",
+        exerciseCategoryId = 1,
+        createdAt = 1000L,
+        completedAt = 2000L,
+        sets = null,
+        reps = null,
+        duration = null,
+        score = 5,
+    ),
+    Exercise(
+        id = 2,
+        name = "Exercise 2",
+        exerciseCategoryId = 1,
+        createdAt = 1000L,
+        completedAt = 2000L,
+        sets = null,
+        reps = null,
+        duration = null,
+        score = 5,
+    ),
+    Exercise(
+        id = 3,
+        name = "Exercise 3",
+        exerciseCategoryId = 1,
+        createdAt = 1000L,
+        completedAt = 2000L,
+        sets = null,
+        reps = null,
+        duration = null,
+        score = 5,
+    ),
+    Exercise(
+        id = 4,
+        name = "Exercise 4",
+        exerciseCategoryId = 1,
+        createdAt = 1000L,
+        completedAt = 2000L,
+        sets = null,
+        reps = null,
+        duration = null,
+        score = 5,
+    ),
+    Exercise(
+        id = 5,
+        name = "Exercise 5",
+        exerciseCategoryId = 1,
+        createdAt = 1000L,
+        completedAt = 2000L,
+        sets = null,
+        reps = null,
+        duration = null,
+        score = 5,
+    ),
+)
---
+++ feature/exercisehistory/src/test/java/com/example/fitness/feature/exercisehistory/ExerciseHistoryViewModelTest.kt
@@ -0,0 +1,50 @@
+package com.example.fitness.feature.exercisehistory
+
+import com.example.fitness.core.testing.data.ExercisesData
+import com.example.fitness.core.testing.utils.MainDispatcherRule
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations.openMocks
+import org.mockito.kotlin.verify
+
+class ExerciseHistoryViewModelTest {
+
+    @get:Rule val mainDispatcherRule = MainDispatcherRule()
+
+    @Mock private lateinit var interactor: ExerciseHistoryInteractor
+
+    private lateinit var viewModel: ExerciseHistoryViewModel
+
+    @Before
+    fun setup() {
+        openMocks(this)
+
+        viewModel = ExerciseHistoryViewModel(
+            interactor = interactor
+        )
+    }
+
+    @Test
+    fun testUiStateLoading() = runTest {
+        assertEquals(
+            ExerciseHistoryUiState.Loading,
+            viewModel.uiState.value
+        )
+    }
+
+    @Test
+    fun testUiStateSuccess() = runTest {
+        val exercises = ExercisesData
+        `when`(interactor.observeExercises())
+            .thenReturn(flowOf(exercises))
+
+        verify(interactor).observeExercises()
+    }
+}
