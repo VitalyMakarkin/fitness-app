e6bd515a946b7037d6b7e01f8c4f55e6a23ad71f
MakarkinVM <makarkinvm@nknh.sibur.ru>
2023-09-28 13:05:26 +0300

    Update tests

---
+++ core/testing/src/main/java/com/example/fitness/core/testing/data/ExerciseGroupsData.kt
@@ -0,0 +1,21 @@
+package com.example.fitness.core.testing.data
+
+import com.example.fitness.core.model.ExerciseGroup
+
+val ExerciseGroupsData = listOf(
+    ExerciseGroup(
+        id = 1,
+        name = "Exercise Group 1",
+        exercises = emptyList(),
+    ),
+    ExerciseGroup(
+        id = 2,
+        name = "Exercise Group 2",
+        exercises = emptyList(),
+    ),
+    ExerciseGroup(
+        id = 3,
+        name = "Exercise Group 3",
+        exercises = emptyList(),
+    ),
+)
---
+++ feature/exercisegroups/src/test/java/com/example/fitness/feature/exercisegroups/ExerciseGroupsViewModelTest.kt
@@ -0,0 +1,50 @@
+package com.example.fitness.feature.exercisegroups
+
+import com.example.fitness.core.testing.data.ExerciseGroupsData
+import com.example.fitness.core.testing.utils.MainDispatcherRule
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations.openMocks
+import org.mockito.kotlin.verify
+
+class ExerciseGroupsViewModelTest {
+
+    @get:Rule val mainDispatcherRule = MainDispatcherRule()
+
+    @Mock private lateinit var interactor: ExerciseGroupsInteractor
+
+    private lateinit var viewModel: ExerciseGroupsViewModel
+
+    @Before
+    fun setup() {
+        openMocks(this)
+
+        viewModel = ExerciseGroupsViewModel(
+            interactor = interactor
+        )
+    }
+
+    @Test
+    fun testUiStateLoading() = runTest {
+        assertEquals(
+            ExerciseGroupsUiState.Loading,
+            viewModel.uiState.value,
+        )
+    }
+
+    @Test
+    fun testUiStateSuccess() = runTest {
+        val groups = ExerciseGroupsData
+        Mockito.`when`(interactor.observeExerciseGroups())
+            .thenReturn(flowOf(groups))
+
+        verify(interactor).observeExerciseGroups()
+    }
+}
