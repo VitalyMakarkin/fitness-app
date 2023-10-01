0db1fa8349501735a242da2234c1ca3251c900c1
MakarkinVM <makarkinvm@nknh.sibur.ru>
2023-10-01 22:11:59 +0300

    Update ui

--- app/src/main/java/com/example/fitness/ui/theme/Color.kt
+++ app/src/main/java/com/example/fitness/ui/theme/Color.kt
@@ -2,14 +2,14 @@ package com.example.fitness.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val LightPrimaryColor = Color(0xFF536DFE)
-val LightSecondaryColor = Color(0xFF536DFE)
-val LightTertiaryColor = Color(0xFF536DFE)
-val LightOnPrimary = Color(0xFFEAEDFF)
-val LightSurfaceVariant = Color(0xFFEAEDFF)
+val LightPrimaryColor = Color(0xFF202020)
+val LightSecondaryColor = Color(0xFF202020)
+val LightTertiaryColor = Color(0xFF202020)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightSurfaceVariant = Color(0xFFC8C8C8)
 
 val DarkPrimaryColor = Color(0xFF616161)
-val DarkSecondaryColor = Color(0xFF536DFE)
-val DarkTertiaryColor = Color(0xFF536DFE)
+val DarkSecondaryColor = Color(0xFF303D85)
+val DarkTertiaryColor = Color(0xFF303D85)
 val DarkOnPrimary = Color(0xFF383C52)
 val DarkSurfaceVariant = Color(0xFF383C52)
