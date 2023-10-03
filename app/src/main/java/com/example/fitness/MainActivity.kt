2e2599efdbbb721de86aa24da1a10320535a21df
MakarkinVM <makarkinvm@nknh.sibur.ru>
2023-10-03 22:38:50 +0300

    Fix detekt

--- app/src/main/java/com/example/fitness/MainActivity.kt
+++ app/src/main/java/com/example/fitness/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.fitness
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.runtime.CompositionLocalProvider
 import com.example.fitness.ui.FitnessApp
 import com.example.fitness.ui.theme.FitnessTheme
@@ -12,7 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
+//    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
