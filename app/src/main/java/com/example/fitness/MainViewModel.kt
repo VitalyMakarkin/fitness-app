cdd5d7732324a015aed0691defa3b5eeb5d66d2f
MakarkinVM <makarkinvm@nknh.sibur.ru>
2023-10-03 22:59:30 +0300

    Fix lint

--- app/src/main/java/com/example/fitness/MainViewModel.kt
+++ app/src/main/java/com/example/fitness/MainViewModel.kt
@@ -1,35 +1,28 @@
 package com.example.fitness
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
 
-    val uiState: StateFlow<MainUiState> = mainUiState().stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5_000),
-        initialValue = MainUiState.Loading
-    )
+//    val uiState: StateFlow<MainUiState> = mainUiState().stateIn(
+//        scope = viewModelScope,
+//        started = SharingStarted.WhileSubscribed(5_000),
+//        initialValue = MainUiState.Loading
+//    )
 }
 
 // TODO: update bottom bar if there is a new notification
-private fun mainUiState(): Flow<MainUiState> {
-    return flow {
-        delay(2_000)
-        emit(MainUiState.Success)
-    }
-}
+//private fun mainUiState(): Flow<MainUiState> {
+//    return flow {
+//        delay(2_000)
+//        emit(MainUiState.Success)
+//    }
+//}
 
-sealed interface MainUiState {
-    object Success : MainUiState
-    object Loading : MainUiState
-}
+//sealed interface MainUiState {
+//    object Success : MainUiState
+//    object Loading : MainUiState
+//}
